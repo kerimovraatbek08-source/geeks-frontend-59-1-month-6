6ee680ea85241f264713303907f2d32a598d978e
Raatbek <public@mail.ru>
2026-06-11 14:58:59 +0600

    fix aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import ProductForm from "./components/ProductForm";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import ProductList from "./components/ProductList";
+import { fetchUsers } from "./features/usersSlice";
+
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { users, loading, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h1>Загрузка...</h1>;
+  }
+
+  if (error) {
+    return <h1>Ошибка: {error}</h1>;
+  }
+
   return (
-    <div>
-      <h1>Каталог товаров</h1>
+    <div className="container">
+      <h1>Пользователи</h1>
 
-      <ProductForm />
+      <div className="cards">
+        {users.map((user) => (
+          <div className="card" key={user.id}>
+            <h2>{user.name}</h2>
 
-      <ProductList />
+            <p>Email: {user.email}</p>
+
+            <p>Телефон: {user.phone}</p>
+
+            <p>Город: {user.address.city}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom/client";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import App from "./App.jsx";
 
-const queryClient = new QueryClient();
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
+  <Provider store={store}>
     <App />
-  </QueryClientProvider>,
+  </Provider>,
 );
